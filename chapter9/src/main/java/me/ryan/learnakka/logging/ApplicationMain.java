f2d73b347955a09d330219993a174a78cad073e9
mogongzi <superrenyuan@hotmail.com>
2021-01-21 13:29:52 +0800

    add thread sleep for making async call clearly

--- chapter9/src/main/java/me/ryan/learnakka/logging/ApplicationMain.java
+++ chapter9/src/main/java/me/ryan/learnakka/logging/ApplicationMain.java
@@ -5,13 +5,16 @@ import akka.actor.ActorSystem;
 
 public class ApplicationMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ActorSystem system = ActorSystem.create("MyActorSystem");
 
         ActorRef pingActor = system.actorOf(LoggingActor.props(), "pingActor");
         pingActor.tell("message1", null);
+        Thread.sleep(2000L);
         pingActor.tell("message2", null);
+        Thread.sleep(2000L);
         pingActor.tell("message3", null);
+        Thread.sleep(2000L);
         system.terminate();
     }
 }
